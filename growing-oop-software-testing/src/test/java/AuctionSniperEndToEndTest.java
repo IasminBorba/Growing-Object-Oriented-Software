08df5a46d8a088a4eb8724cfdecf074f0c6c0725
icborba <icborba@sefaz.al.gov.br>
2024-11-11 11:44:55 -0300

    feat: Implementa o primeiro teste

---
+++ growing-oop-software-testing/src/test/java/AuctionSniperEndToEndTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.*;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");  //leilão
+    private final ApplicationRunner application = new ApplicationRunner(); //sniper
+
+    @Test
+    public void testSniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem(); //Inicia o leilão
+        application.startBiddingIn(auction); //Sniper iniciando a participação no leilão
+        auction.hasReceivedJoinRequestFromSniper(); //o leilão recebeu a solicitação de participação do sniper??
+        auction.announceClosed(); //fechamento do leilão
+        application.showsSniperHasLostAuction(); //sniper perdeu o leilão
+    }
+
+    @AfterEach
+    public void stopAuction() {
+        auction.stop();
+    }
+    @AfterEach
+    public void stopApplication() {
+        application.stop();
+    }
+}
