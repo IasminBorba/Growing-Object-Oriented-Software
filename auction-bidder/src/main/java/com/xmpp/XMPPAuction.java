5d1597dc43322ae3a84850d3cecce01ec4914b9b
icborba <icborba@sefaz.al.gov.br>
2024-12-09 14:14:30 -0300

    fix: Corrige erros nos testes de ponta a ponta

--- auction-bidder/src/main/java/com/xmpp/XMPPAuction.java
+++ auction-bidder/src/main/java/com/xmpp/XMPPAuction.java
@@ -15,10 +15,33 @@ public class XMPPAuction implements Auction {
     public static final String JOIN_COMMAND_FORMAT = "SOLVersion: 1.1; Command: JOIN;";
     public static final String BID_COMMAND_FORMAT = "SOLVersion: 1.1; Command: BID; Price: %d;";
 
+//    public XMPPAuction(XMPPConnection connection, String auctionId) {
+//        chat = connection.getChatManager().createChat(auctionId,
+//                new AuctionMessageTranslator(connection.getUser(),
+//                        auctionEventListeners.announce()));
+//    }
+
     public XMPPAuction(XMPPConnection connection, String auctionId) {
-        chat = connection.getChatManager().createChat(auctionId,
-                new AuctionMessageTranslator(connection.getUser(),
-                        auctionEventListeners.announce()));
+        AuctionMessageTranslator translator = translatorFor(connection);
+        this.chat = connection.getChatManager().createChat(auctionId, translator);
+        addAuctionEventListener(chatDisconnectorFor(translator));
+    }
+
+    private AuctionMessageTranslator translatorFor(XMPPConnection connection) {
+        return new AuctionMessageTranslator(connection.getUser(),
+                auctionEventListeners.announce());
+    }
+
+    private AuctionEventListener chatDisconnectorFor(final AuctionMessageTranslator translator) {
+        return new AuctionEventListener() {
+            public void auctionFailed() {
+                chat.removeMessageListener(translator);
+            }
+            public void auctionClosed() {}
+
+            @Override
+            public void currentPrice(int price, int increment, PriceSource priceSource) {}
+        };
     }
 
     @Override
